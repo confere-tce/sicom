c2496245110529bb62e594daf78accfac18a4ad1
confere-tce <147074932+confere-tce@users.noreply.github.com>
2023-10-05 20:14:39 -0300

    Add files via upload
    Alteracao leitura e insercao csv para pandas

--- app.py
+++ app.py
@@ -1,17 +1,21 @@
 import os
 import csv
+import pandas as pd
 from funcoes import criar_pasta_usuario, deletando_pasta
 from flask import Flask, render_template, request, url_for, redirect
 from zipfile import ZipFile
+from sqlalchemy import create_engine
 import connection
 
 app = Flask(__name__)
 app.config['UPLOAD_FOLDER'] = 'uploads'
 
+engine = create_engine('postgresql://jsmcfbqq:dzYLD0UV56ksursrQrP4fHMi_f1X116e@silly.db.elephantsql.com/jsmcfbqq')
+
 if not os.path.exists(app.config['UPLOAD_FOLDER']):
     os.makedirs(app.config['UPLOAD_FOLDER'])
 
-@app.route('/')
+@app.route('/') 
 def index():
     return render_template('index.html')
 
@@ -51,6 +55,9 @@ def upload_file():
         with ZipFile(filename, 'r') as zip:
             zip.extractall(pasta_usuario) 
 
+        nomes_colunas = ['seq1', 'seq2', 'seq3', 'seq4', 'seq5', 'seq6', 'seq7', 'seq8', 'seq9', 'seq10', 'seq11', 'seq12', 'seq13', 'seq14', 'seq15', 'seq16', 'seq17', 'seq18', 'seq19', 'seq20', 'seq21', 'seq22', 'seq23', 'seq24', 'seq25', 'seq26', 'seq27', 'seq28', 'seq29', 'seq30', 'seq31', 'seq32', 'seq33', 'seq34', 'seq35', 'seq36', 'seq37', 'seq38', 'seq39', 'seq40']
+        dtypes = {coluna: str for coluna in range(30)}  
+
         # Carregando os arquivos da pasta
         for arq in os.listdir(pasta_usuario):
             if arq.endswith('.csv'):
@@ -60,40 +67,51 @@ def upload_file():
                 nome_arquivo = arq[:x]
 
                 processa = False
-                # if nome_arquivo == 'AEX' or \
-                #     nome_arquivo == 'ALQ' or \
-                #     nome_arquivo == 'ANL' or \
-                #     nome_arquivo == 'AOB' or \
-                #     nome_arquivo == 'AOC' or \
-                #     nome_arquivo == 'AOP' or \
-                #     nome_arquivo == 'ARC' or \
-                #     nome_arquivo == 'CAIXA' or \
-                #     nome_arquivo == 'CTB' or \
-                #     nome_arquivo == 'CUTE' or \
-                #     nome_arquivo == 'EMP' or \
-                #     nome_arquivo == 'EXT' or \
-                #     nome_arquivo == 'IDE' or \
-                #     nome_arquivo == 'IDERP' or \
-                #     nome_arquivo == 'LQD' or \
-                #     nome_arquivo == 'OBELAC' or \
-                #     nome_arquivo == 'OPS' or \
-                #     nome_arquivo == 'ORGAO' or \
-                #     nome_arquivo == 'REC':
-                if nome_arquivo == 'ALQ' :
-                    processa = True
+                if nome_arquivo == 'AEX' or \
+                   nome_arquivo == 'ALQ' or \
+                   nome_arquivo == 'ANL' or \
+                   nome_arquivo == 'AOB' or \
+                   nome_arquivo == 'AOC' or \
+                   nome_arquivo == 'AOP' or \
+                   nome_arquivo == 'ARC' or \
+                   nome_arquivo == 'CAIXA' or \
+                   nome_arquivo == 'CTB' or \
+                   nome_arquivo == 'CUTE' or \
+                   nome_arquivo == 'EMP' or \
+                   nome_arquivo == 'EXT' or \
+                   nome_arquivo == 'IDE' or \
+                   nome_arquivo == 'IDERP' or \
+                   nome_arquivo == 'LQD' or \
+                   nome_arquivo == 'OBELAC' or \
+                   nome_arquivo == 'OPS' or \
+                   nome_arquivo == 'ORGAO' or \
+                   nome_arquivo == 'REC':
+                       processa = True
 
                 if processa:
 
                     arq_completo = os.path.join(pasta_usuario, arq)
 
-                    with open(arq_completo, mode='r') as arq_csv:
-                        linha = csv.reader(arq_csv, delimiter=';')
-                        for colunas in linha:
+    
+                     # Carregar o arquivo CSV em um DataFrame do Pandas
+                    df = pd.read_csv(arq_completo, delimiter=';', encoding='iso-8859-1', header=None, names=nomes_colunas, dtype=dtypes)
 
-                            colunas.extend([None] * (30 - len(colunas)))
-                            print(colunas)
+                    # Preencher com None para completar até 30 colunas
+                    df = df.reindex(columns=[*df.columns, *range(30 - len(df.columns))])
 
-                            connection.insere('USUARIO', 'AM', nome_arquivo, colunas)
+                    # Manter apenas as 30 primeiras colunas
+                    df = df.iloc[:, :30]
+
+                    # Substituir NaN por None
+                    df = df.where(pd.notna(df), None)
+
+                    df.insert(0, 'arquivo', nome_arquivo) 
+                    df.insert(0, 'modulo', 'AM')  
+                    df.insert(0, 'usuario', 'USUARIO')  
+
+                    print(df)
+
+                    df.to_sql('tce_sicom', engine, if_exists='append', index=False)
 
         # Deletando a pasta depois do processamento
         deletando_pasta(pasta_usuario)
