3cf03402d7158fe8a8319d58720bbb77b6c768ef
ranzatti <ranzatti@sonner.com.br>
2023-10-19 12:12:18 -0300

    Graficos

--- Home.py
+++ Home.py
@@ -22,12 +22,10 @@ if st.session_state.cod_municipio_AM:
     with st.container():
 
         dados = graficoEmpenhoReceita(st.experimental_user, st.session_state.ano)
-        valores = [int(dados[0][0]), int(dados[0][1])]
+        valores = [float(dados[0][0]), float(dados[0][1])]
 
         col1, col2, col3 = st.columns(3)
         with col1:
-            st.write("Empenho x Receita")
-
             source = pd.DataFrame({
                 'Tipo': ['Empenho', 'Receita'],
                 'Valores em R$': valores
@@ -35,50 +33,54 @@ if st.session_state.cod_municipio_AM:
 
             bar_chart = alt.Chart(source).mark_bar().encode(
                 x='Tipo',
-                y='Valores em R$',
+                y='Valores em R$:Q',
                 color=alt.Color('Tipo', scale=alt.Scale( domain=['Empenho', 'Receita'], range=['red', 'green']))
             ).properties(
-                width=300,
+                title='Empenho x Receitas',
+                width=300, 
                 height=300
             )
 
             st.altair_chart(bar_chart, use_container_width=True)
-        
+
         with col2:
-            st.markdown("<p style='color: red; font-weight: bold'>Empenho x Receita</p>", unsafe_allow_html=True)
+            # st.markdown("<p style='color: red; font-weight: bold'>Empenho x Receita</p>", unsafe_allow_html=True) -> de exemplo
 
             source = pd.DataFrame({
                 'Tipo': ['Empenho', 'Receita'],
                 'Valores em R$': valores
             })
+
             bar_chart = alt.Chart(source).mark_arc().encode(
                 theta="Valores em R$",
                 color="Tipo"
             ).properties(
+                title='Empenho x Receitas',
                 width=300,
                 height=300
             )
 
             st.altair_chart(bar_chart, use_container_width=True)
         with col3:
-
-            st.markdown("<p style='color: blue; font-weight: bold'>Empenho x Receita</p>", unsafe_allow_html=True)
-            # st.write("Empenho x Receita")
-
             source = pd.DataFrame({
                 'Tipo': ['Empenho', 'Receita'],
                 'Valores em R$': valores
             })
-            
+
             bar_chart = alt.Chart(source).mark_arc(innerRadius=50).encode(
                 # theta="Valores em R$",
                 # color="Tipo:N",
-                theta=alt.Theta(field="Valores em R$", type="quantitative", stack=True, scale=alt.Scale(type="linear",rangeMax=1.5708, rangeMin=-1.5708 )),
-                # color=alt.Color(field="Tipo", type="nominal"),
-                color=alt.Color('Tipo', scale=alt.Scale( domain=['Empenho', 'Receita'], range=['red', 'green']))
+                theta=alt.Theta(
+                    field="Valores em R$", 
+                    type="quantitative", 
+                    stack=True, 
+                    scale=alt.Scale(type="linear", rangeMax=1.5708, rangeMin=-1.5708)),
+                color=alt.Color('Tipo', scale=alt.Scale(
+                    domain=['Empenho', 'Receita'], range=['red', 'green']))
             ).properties(
+                title='Empenho x Receitas',
                 width=300,
                 height=300
             )
-            
+
             st.altair_chart(bar_chart, use_container_width=True)
