d77d0e71ec2cc8e96e1e3868d2073db83893b283
ranzatti <ranzatti@sonner.com.br>
2023-10-19 10:47:31 -0300

    graficos

--- Home.py
+++ Home.py
@@ -2,7 +2,7 @@ import streamlit as st
 from funcoes import init
 from ConsultasSQL import *
 import pandas as pd
-import plotly.express as px
+import altair as alt
 
 st.set_page_config(
     layout="wide",
@@ -10,6 +10,7 @@ st.set_page_config(
 )
 
 init(st)
+
 
 st.subheader("Home", divider='rainbow')
 
@@ -19,32 +20,65 @@ st.experimental_user = st.sidebar.text_input('Usuário', 'USER')
 if st.session_state.cod_municipio_AM:
     ##### EMPENHOS #####
     with st.container():
-        col1, col2, col3 = st.columns(3)
-        with col1:
-            dados = graficoEmpenhoReceita(st.experimental_user, st.session_state.ano)
-            
-            valores = [dados[0][0], dados[0][1]]
-
-            data = {
-                'Tipo ': ['Empenho', 'Receita'],
-                'Valores em R$ ': valores
-            }
-            fig = px.pie(data, names='Tipo ', values='Valores em R$ ', title='Empenhos x Receita', color_discrete_sequence=px.colors.sequential.RdBu)
-            fig.update_layout(autosize=True)
-            st.write(fig)
-        
-        with col2:
-            df = px.data.gapminder().query("year == 2007").query("continent == 'Europe'")
-            df.loc[df['pop'] < 2.e6, 'country'] = 'Other countries' # Represent only large countries
-            fig = px.pie(df, values='pop', names='country', title='Population of European continent')
-            st.write(fig)
-        with col3:
-            df = px.data.tips()
-            fig = px.pie(df, values='tip', names='day')
-            fig.update_layout(autosize=True)
-            st.write(fig)
 
         dados = graficoEmpenhoReceita(st.experimental_user, st.session_state.ano)
-        if dados:
-            valores = [dados[0][0], dados[0][1]]
-            # st.line_chart(pd.DataFrame(data=dados, columns=["Emp", "Rec"]))
+        valores = [int(dados[0][0]), int(dados[0][1])]
+
+        col1, col2, col3 = st.columns(3)
+        with col1:
+            st.write("Empenho x Receita")
+
+            source = pd.DataFrame({
+                'Tipo': ['Empenho', 'Receita'],
+                'Valores em R$': valores
+            })
+
+            bar_chart = alt.Chart(source).mark_bar().encode(
+                x='Tipo',
+                y='Valores em R$',
+                color=alt.Color('Tipo', scale=alt.Scale( domain=['Empenho', 'Receita'], range=['red', 'green']))
+            ).properties(
+                width=300,
+                height=300
+            )
+
+            st.altair_chart(bar_chart, use_container_width=True)
+        
+        with col2:
+            st.markdown("<p style='color: red; font-weight: bold'>Empenho x Receita</p>", unsafe_allow_html=True)
+
+            source = pd.DataFrame({
+                'Tipo': ['Empenho', 'Receita'],
+                'Valores em R$': valores
+            })
+            bar_chart = alt.Chart(source).mark_arc().encode(
+                theta="Valores em R$",
+                color="Tipo"
+            ).properties(
+                width=300,
+                height=300
+            )
+
+            st.altair_chart(bar_chart, use_container_width=True)
+        with col3:
+
+            st.markdown("<p style='color: blue; font-weight: bold'>Empenho x Receita</p>", unsafe_allow_html=True)
+            # st.write("Empenho x Receita")
+
+            source = pd.DataFrame({
+                'Tipo': ['Empenho', 'Receita'],
+                'Valores em R$': valores
+            })
+            
+            bar_chart = alt.Chart(source).mark_arc(innerRadius=50).encode(
+                # theta="Valores em R$",
+                # color="Tipo:N",
+                theta=alt.Theta(field="Valores em R$", type="quantitative", stack=True, scale=alt.Scale(type="linear",rangeMax=1.5708, rangeMin=-1.5708 )),
+                # color=alt.Color(field="Tipo", type="nominal"),
+                color=alt.Color('Tipo', scale=alt.Scale( domain=['Empenho', 'Receita'], range=['red', 'green']))
+            ).properties(
+                width=300,
+                height=300
+            )
+            
+            st.altair_chart(bar_chart, use_container_width=True)
